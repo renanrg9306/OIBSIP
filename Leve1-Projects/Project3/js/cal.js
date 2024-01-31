d54fb9ddaee6047a4718c557223abc62811fc0bd
Renan Rosales <renanrg9306@gmail.com>
2024-01-31 16:50:10 -0600

    Terminado

--- Leve1-Projects/Project3/js/cal.js
+++ Leve1-Projects/Project3/js/cal.js
@@ -1,33 +1,54 @@
-var temp = document.getElementById('degree');
 
-var conver = document.getElementById('temptype');
-console.log(conver.value);
+let resultinput = document.getElementById('tempresult');
 
 
-function tempConvertida(conversion,temperatura){
-    var temp = 0;
+function tempConvertida(){
+    try{
+        let result = 0;
+        let temp = parseInt(document.getElementById('degrees').value); 
+        let conver = document.getElementById('temptype').value; 
 
-    switch(conversion){
-        case 'celsius':
-            temp = 5/9(temperatura-32);
-            console.log(temp);
-            break;
+        if (isNaN(temp)){
+            throw new Error("El dato ingresado no fue un numeor valido.");
 
-        case 'farhenheit':
-            break;
-
-        case 'celsiustokelvin':
-            break;
-
-        case 'farhenheittokelvin':
-            break;
+        }
     
-        default:
-            console.log('Conversion Invalida');
+        switch(conver){
+            case 'celsius':
+                result = 5/9*(temp-32);
+                resultinput.value = result.toFixed(1);
+                break;
+    
+            case 'farhenheit':
+                result = 9/5*temp + 32;
+                resultinput.value = result.toFixed(1);
+               
+                break;
+    
+            case 'celsiustokelvin':
+                result =  temp + 273.15;
+                resultinput.value = result.toFixed(1);
+                break;
+    
+            case 'farhenheittokelvin':
+                result = (temp - 32)/1.8 + 273.15;
+                resultinput.value = result.toFixed(1);
+                break;
+        
+            default:
+                console.log('Conversion Invalida');
+        }
+
+    }catch(error){
+        console.log(`Se produjo el siguiente error ${error}`);
+        alert(error);
     }
+   
 }
 
 
+
+
 var calcularTemp = document.getElementById('btnCalcular');
-calcularTemp.addEventListener('click',tempConvertida(conver,temp));
+calcularTemp.addEventListener('click',tempConvertida);
 
